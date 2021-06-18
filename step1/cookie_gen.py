5b85063b993ed0a225809347e8add4310fc7d3d3
Harry Maraziaris <sdi1800105@di.uoa.gr>
2021-06-18 23:00:33 +0300

    Final changes to steps 1 and 2

--- step1/cookie_gen.py
+++ step1/cookie_gen.py
@@ -14,10 +14,12 @@ def encode_to_sha256(str):
 	sha256_str = sha256(b).hexdigest()
 	return sha256_str
 
-if __name__ == '__main__':
+def main():
 	code = input()
 	code_sha256 = encode_to_sha256(code)
 	mid = code + ':' + code_sha256
 	final = encode_to_b64(mid)
 	print(final)
-	exit(0)
+
+if __name__ == '__main__':
+	main()
